a5b92d4c4a9dd8a176fb9bd129c3feda132c8d67
RenFei <i@renfei.net>
2023-05-25 17:12:32 +0800

    feat: 添加用户接口

--- src/main/java/net/renfei/unifiauth/sdk/oauth2/Scopes.java
+++ src/main/java/net/renfei/unifiauth/sdk/oauth2/Scopes.java
@@ -6,6 +6,64 @@ package net.renfei.unifiauth.sdk.oauth2;
  * @author renfei
  */
 public final class Scopes {
+    /**
+     * 获取用户的基本信息
+     */
     public static final String PROFILE = "profile";
+    /**
+     * 获取用户的角色列表
+     */
+    public static final String ROLES = "roles";
+    /**
+     * 获取用户拥有的菜单树
+     */
+    public static final String MENUS = "menus";
+    /**
+     * 查询用户资料
+     */
     public static final String USER_READ = "user.read";
+    /**
+     * 创建用户
+     */
+    public static final String USER_CREATE = "user.create";
+    /**
+     * 修改用户
+     */
+    public static final String USER_UPDATE = "user.update";
+    /**
+     * 删除用户
+     */
+    public static final String USER_DELETE = "user.delete";
+    /**
+     * 查询角色列表
+     */
+    public static final String ROLE_READ = "role.read";
+    /**
+     * 创建角色
+     */
+    public static final String ROLE_CREATE = "role.create";
+    /**
+     * 修改角色
+     */
+    public static final String ROLE_UPDATE = "role.update";
+    /**
+     * 删除角色
+     */
+    public static final String ROLE_DELETE = "role.delete";
+    /**
+     * 查询菜单列表
+     */
+    public static final String MENU_READ = "menu.read";
+    /**
+     * 创建菜单
+     */
+    public static final String MENU_CREATE = "menu.create";
+    /**
+     * 修改菜单
+     */
+    public static final String MENU_UPDATE = "menu.update";
+    /**
+     * 删除菜单
+     */
+    public static final String MENU_DELETE = "menu.delete";
 }
--- src/main/java/net/renfei/unifiauth/sdk/service/UserService.java
+++ src/main/java/net/renfei/unifiauth/sdk/service/UserService.java
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import net.renfei.unifiauth.sdk.UnifiAuthClient;
 import net.renfei.unifiauth.sdk.constant.HttpStatus;
 import net.renfei.unifiauth.sdk.entity.ApiResult;
+import net.renfei.unifiauth.sdk.entity.MenuDetail;
+import net.renfei.unifiauth.sdk.entity.RoleDetail;
 import net.renfei.unifiauth.sdk.entity.UserProfile;
 import net.renfei.unifiauth.sdk.utils.HttpClientUtils;
 import net.renfei.unifiauth.sdk.utils.JSONUtils;
 
 import java.net.URI;
+import java.util.List;
 
 /**
  * @author renfei
@@ -20,6 +23,13 @@ public class UserService {
         this.UNIFI_AUTH_CLIENT = unifiAuthClient;
     }
 
+    /**
+     * 查询用户基础信息（SCOPE:profile）
+     *
+     * @param token
+     * @return
+     * @throws Exception
+     */
     public UserProfile queryUserProfile(String token) throws Exception {
         StringBuilder url = new StringBuilder(UNIFI_AUTH_CLIENT.UNIFI_AUTH_SERVER_URI);
         if (!UNIFI_AUTH_CLIENT.UNIFI_AUTH_SERVER_URI.endsWith(UnifiAuthClient.URI_SEPARATOR)) {
@@ -30,6 +40,64 @@ public class UserService {
         HttpClientUtils httpClientUtils = new HttpClientUtils();
         String result = httpClientUtils.get(uri, token);
         ApiResult<UserProfile> apiResult = JSONUtils.json2pojo(result, new TypeReference<ApiResult<UserProfile>>() {
+        });
+        if (apiResult.getCode() == HttpStatus.SUCCESS) {
+            return apiResult.getData();
+        } else {
+            throw new RuntimeException(apiResult.getMessage());
+        }
+    }
+
+    /**
+     * 查询用户的角色列表（SCOPE:roles）
+     * <br>
+     * 如需使用此功能，需要在统一权限平台上对用户进行角色设置
+     * <br>
+     * 每个用户在每个子系统中拥有的权限和菜单都不相同
+     *
+     * @param token
+     * @return
+     * @throws Exception
+     */
+    public List<RoleDetail> queryUserRoleList(String token) throws Exception {
+        StringBuilder url = new StringBuilder(UNIFI_AUTH_CLIENT.UNIFI_AUTH_SERVER_URI);
+        if (!UNIFI_AUTH_CLIENT.UNIFI_AUTH_SERVER_URI.endsWith(UnifiAuthClient.URI_SEPARATOR)) {
+            url.append(UnifiAuthClient.URI_SEPARATOR);
+        }
+        url.append("resource/roles");
+        URI uri = new URI(url.toString());
+        HttpClientUtils httpClientUtils = new HttpClientUtils();
+        String result = httpClientUtils.get(uri, token);
+        ApiResult<List<RoleDetail>> apiResult = JSONUtils.json2pojo(result, new TypeReference<ApiResult<List<RoleDetail>>>() {
+        });
+        if (apiResult.getCode() == HttpStatus.SUCCESS) {
+            return apiResult.getData();
+        } else {
+            throw new RuntimeException(apiResult.getMessage());
+        }
+    }
+
+    /**
+     * 查询用户拥有的菜单树（SCOPE:menus）
+     * <br>
+     * 如需使用此功能，需要在统一权限平台上对用户进行角色设置
+     * <br>
+     * 每个用户在每个子系统中拥有的权限和菜单都不相同
+     *
+     * @param token
+     * @return
+     * @throws Exception
+     */
+    public List<MenuDetail> queryUserMenuTree(String token) throws Exception {
+        StringBuilder url = new StringBuilder(UNIFI_AUTH_CLIENT.UNIFI_AUTH_SERVER_URI);
+        if (!UNIFI_AUTH_CLIENT.UNIFI_AUTH_SERVER_URI.endsWith(UnifiAuthClient.URI_SEPARATOR)) {
+            url.append(UnifiAuthClient.URI_SEPARATOR);
+        }
+        url.append("resource/menus");
+        URI uri = new URI(url.toString());
+        HttpClientUtils httpClientUtils = new HttpClientUtils();
+        String result = httpClientUtils.get(uri, token);
+        ApiResult<List<MenuDetail>> apiResult = JSONUtils.json2pojo(result, new TypeReference<ApiResult<List<MenuDetail>>>() {
         });
         if (apiResult.getCode() == HttpStatus.SUCCESS) {
             return apiResult.getData();
